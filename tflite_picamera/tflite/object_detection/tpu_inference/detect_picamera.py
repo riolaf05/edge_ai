565b86b194fdad50879212587351401e365f5216
Rosario Laface <rosario.laface@nttdata.com>
2020-01-22 14:16:54 +0100

    adding tpu accelerator code

---
+++ tflite_picamera/tflite/object_detection/tpu_inference/detect_picamera.py
@@ -0,0 +1,224 @@
+# See https://github.com/tensorflow/examples/tree/master/lite/examples/object_detection/raspberry_pi
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import io
+import re
+import time
+
+from annotation import Annotator
+
+import numpy as np
+import picamera
+
+from PIL import Image
+from tflite_runtime.interpreter import Interpreter
+
+import logging
+import socketserver
+from threading import Condition
+from http import server
+
+from tflite_runtime.interpreter import load_delegate
+
+CAMERA_WIDTH = 640
+CAMERA_HEIGHT = 480
+
+
+def load_labels(path):
+  """Loads the labels file. Supports files with or without index numbers."""
+  with open(path, 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+    labels = {}
+    for row_number, content in enumerate(lines):
+      pair = re.split(r'[:\s]+', content.strip(), maxsplit=1)
+      if len(pair) == 2 and pair[0].strip().isdigit():
+        labels[int(pair[0])] = pair[1].strip()
+      else:
+        labels[row_number] = pair[0].strip()
+  return labels
+
+
+def set_input_tensor(interpreter, image):
+  """Sets the input tensor."""
+  tensor_index = interpreter.get_input_details()[0]['index']
+  input_tensor = interpreter.tensor(tensor_index)()[0]
+  input_tensor[:, :] = image
+
+
+def get_output_tensor(interpreter, index):
+  """Returns the output tensor at the given index."""
+  output_details = interpreter.get_output_details()[index]
+  tensor = np.squeeze(interpreter.get_tensor(output_details['index']))
+  return tensor
+
+
+def detect_objects(interpreter, image, threshold):
+  """Returns a list of detection results, each a dictionary of object info."""
+  set_input_tensor(interpreter, image)
+  interpreter.invoke()
+
+  # Get all output details
+  boxes = get_output_tensor(interpreter, 0)
+  classes = get_output_tensor(interpreter, 1)
+  scores = get_output_tensor(interpreter, 2)
+  count = int(get_output_tensor(interpreter, 3))
+
+  results = []
+  for i in range(count):
+    if scores[i] >= threshold:
+      result = {
+          'bounding_box': boxes[i],
+          'class_id': classes[i],
+          'score': scores[i]
+      }
+      results.append(result)
+  return results
+
+
+def annotate_objects(annotator, results, labels):
+  """Draws the bounding box and label for each object in the results."""
+  for obj in results:
+    # Convert the bounding box figures from relative coordinates
+    # to absolute coordinates based on the original resolution
+    ymin, xmin, ymax, xmax = obj['bounding_box']
+    xmin = int(xmin * CAMERA_WIDTH)
+    xmax = int(xmax * CAMERA_WIDTH)
+    ymin = int(ymin * CAMERA_HEIGHT)
+    ymax = int(ymax * CAMERA_HEIGHT)
+
+    # Overlay the box, label, and score on the camera preview
+    annotator.bounding_box([xmin, ymin, xmax, ymax])
+    annotator.text([xmin, ymin],
+                   '%s\n%.2f' % (labels[obj['class_id']], obj['score']))
+
+PAGE="""\
+<html>
+<head>
+<title>picamera MJPEG streaming demo</title>
+</head>
+<body>
+<h1>PiCamera MJPEG Streaming Demo</h1>
+<img src="stream.mjpg" width="640" height="480" />
+</body>
+</html>
+"""
+
+class StreamingOutput(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
+
+def main():
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument(
+      '--model', help='File path of .tflite file.', required=True)
+  parser.add_argument(
+      '--labels', help='File path of labels file.', required=True)
+  parser.add_argument(
+      '--threshold',
+      help='Score threshold for detected objects.',
+      required=False,
+      type=float,
+      default=0.4)
+  args = parser.parse_args()
+
+  labels = load_labels(args.labels)
+  interpreter = Interpreter(args.model experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
+  interpreter.allocate_tensors()
+  _, input_height, input_width, _ = interpreter.get_input_details()[0]['shape']
+
+  with picamera.PiCamera(resolution=(CAMERA_WIDTH, CAMERA_HEIGHT), framerate=30) as camera:
+    print("Start recording..")
+    output = StreamingOutput()
+    camera.start_preview()
+    camera.start_recording(output, format='mjpeg')
+    try:
+
+      address = ('', 8000)
+      server = StreamingServer(address, StreamingHandler)
+      server.serve_forever()
+      stream = io.BytesIO()
+      
+      annotator = Annotator(camera)
+      
+      for _ in camera.capture_continuous(stream, format='jpeg', use_video_port=True):
+        stream.seek(0)
+        image = Image.open(stream).convert('RGB').resize((input_width, input_height), Image.ANTIALIAS)
+        start_time = time.monotonic()
+        results = detect_objects(interpreter, image, args.threshold)
+        elapsed_ms = (time.monotonic() - start_time) * 1000
+
+        annotator.clear()
+        annotate_objects(annotator, results, labels)
+        annotator.text([5, 0], '%.1fms' % (elapsed_ms))
+        annotator.update()
+
+        stream.seek(0)
+        stream.truncate()
+
+    finally:
+      camera.stop_recording()
+      camera.stop_preview()
+
+
+if __name__ == '__main__':
+  main()
