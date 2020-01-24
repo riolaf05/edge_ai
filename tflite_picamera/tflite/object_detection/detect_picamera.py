5eec1c8664fe2246971890bb277b3b989e579a71
Rosario Laface <rosario.laface@nttdata.com>
2020-01-24 18:15:02 +0100

    Update detect_picamera.py

--- tflite_picamera/tflite/object_detection/detect_picamera.py
+++ tflite_picamera/tflite/object_detection/detect_picamera.py
@@ -1,5 +1,3 @@
-# python3
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -16,11 +14,6 @@ import picamera
 
 from PIL import Image
 from tflite_runtime.interpreter import Interpreter
-
-import logging
-import socketserver
-from threading import Condition
-from http import server
 
 CAMERA_WIDTH = 640
 CAMERA_HEIGHT = 480
@@ -93,77 +86,6 @@ def annotate_objects(annotator, results, labels):
     annotator.text([xmin, ymin],
                    '%s\n%.2f' % (labels[obj['class_id']], obj['score']))
 
-PAGE="""\
-<html>
-<head>
-<title>picamera MJPEG streaming demo</title>
-</head>
-<body>
-<h1>PiCamera MJPEG Streaming Demo</h1>
-<img src="stream.mjpg" width="640" height="480" />
-</body>
-</html>
-"""
-
-class StreamingOutput(object):
-    def __init__(self):
-        self.frame = None
-        self.buffer = io.BytesIO()
-        self.condition = Condition()
-
-    def write(self, buf):
-        if buf.startswith(b'\xff\xd8'):
-            # New frame, copy the existing buffer's content and notify all
-            # clients it's available
-            self.buffer.truncate()
-            with self.condition:
-                self.frame = self.buffer.getvalue()
-                self.condition.notify_all()
-            self.buffer.seek(0)
-        return self.buffer.write(buf)
-
-class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
-    allow_reuse_address = True
-    daemon_threads = True
-
-class StreamingHandler(server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/index.html')
-            self.end_headers()
-        elif self.path == '/index.html':
-            content = PAGE.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
 
 def main():
   parser = argparse.ArgumentParser(
@@ -185,39 +107,32 @@ def main():
   interpreter.allocate_tensors()
   _, input_height, input_width, _ = interpreter.get_input_details()[0]['shape']
 
-with picamera.PiCamera(resolution=(CAMERA_WIDTH, CAMERA_HEIGHT), framerate=30) as camera:
-  print("Start recording..")
-  output = StreamingOutput()
-  camera.start_preview()
-  camera.start_recording(output, format='mjpeg')
+  with picamera.PiCamera(
+      resolution=(CAMERA_WIDTH, CAMERA_HEIGHT), framerate=30) as camera:
+    camera.start_preview()
+    try:
+      stream = io.BytesIO()
+      annotator = Annotator(camera)
+      for _ in camera.capture_continuous(
+          stream, format='jpeg', use_video_port=True):
+        stream.seek(0)
+        image = Image.open(stream).convert('RGB').resize(
+            (input_width, input_height), Image.ANTIALIAS)
+        start_time = time.monotonic()
+        results = detect_objects(interpreter, image, args.threshold)
+        elapsed_ms = (time.monotonic() - start_time) * 1000
 
-  address = ('', 8000)
-  server = StreamingServer(address, StreamingHandler)
-  server.serve_forever()
-  
-  annotator = Annotator(camera)
-  
-  try:
-    stream = io.BytesIO()
-    annotator = Annotator(camera)
-    for _ in camera.capture_continuous(stream, format='jpeg', use_video_port=True):
-      print("test")
-      stream.seek(0)
-      image = Image.open(stream).convert('RGB').resize((input_width, input_height), Image.ANTIALIAS)
-      start_time = time.monotonic()
-      results = detect_objects(interpreter, image, args.threshold)
-      elapsed_ms = (time.monotonic() - start_time) * 1000
+        annotator.clear()
+        annotate_objects(annotator, results, labels)
+        annotator.text([5, 0], '%.1fms' % (elapsed_ms))
+        annotator.update()
 
-      annotator.clear()
-      annotate_objects(annotator, results, labels)
-      annotator.text([5, 0], '%.1fms' % (elapsed_ms))
-      annotator.update()
+        stream.seek(0)
+        stream.truncate()
 
-      stream.seek(0)
-      stream.truncate()
-
-  finally:
-    camera.stop_recording()
-    camera.stop_preview()
+    finally:
+      camera.stop_preview()
 
 
+if __name__ == '__main__':
+  main()
