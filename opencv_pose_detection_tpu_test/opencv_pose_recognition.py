bbb6ca31a6f383b18edac4dc4b0643d52c174950
Rosario Laface <rosario.laface@nttdata.com>
2020-03-14 18:40:41 +0100

    updating

--- opencv_pose_detection_tpu_test/opencv_pose_recognition.py
+++ opencv_pose_detection_tpu_test/opencv_pose_recognition.py
@@ -1,74 +1,221 @@
-#import tensorflow as tf
-from flask import Flask, render_template, Response
-import cv2
+import os
+import io
 import time
+import base64
+import argparse
+import logging
+import socketserver
+from http import server
+from threading import Condition
+
+import picamera
+import cv2
+from PIL import Image
+
 import numpy as np
-import matplotlib.pyplot as plt
+from edgetpu.detection.engine import DetectionEngine
+from pose_engine import PoseEngine
 
-#lock = threading.Lock()
-# initialize a flask object
-app = Flask(__name__)
+# Parameters
+AUTH_USERNAME = os.environ.get('AUTH_USERNAME', 'pi')
+AUTH_PASSWORD = os.environ.get('AUTH_PASSWORD', 'picamera')
+AUTH_BASE64 = base64.b64encode('{}:{}'.format(AUTH_USERNAME, AUTH_PASSWORD).encode('utf-8'))
+BASIC_AUTH = 'Basic {}'.format(AUTH_BASE64.decode('utf-8'))
+RESOLUTION = os.environ.get('RESOLUTION', '800x600').split('x')
+RESOLUTION_X = int(RESOLUTION[0])
+RESOLUTION_Y = int(RESOLUTION[1])
+FRAMERATE = int(os.environ.get('FRAMERATE', '30'))
+ROTATION = int(os.environ.get('ROTATE', 0))
+HFLIP = os.environ.get('HFLIP', 'false').lower() == 'true'
+VFLIP = os.environ.get('VFLIP', 'true').lower() == 'true'
 
-def pose_detect():
-    #with tf.Session() as sess:
-        #model_cfg, model_outputs = posenet.load_model(args.model, sess)
-        #output_stride = model_cfg['output_stride']
-        
-        inWidth = 368
-        inHeight = 368
-        cam_width = 1280
-        cam_height = 720
-        cam_id = 0
-        video_file = None
-        threshold = 0.1
-        net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+PAGE = """\
+<html>
+<head>
+<title>edgeTPU Object Detection</title>
+</head>
+<body>
+<img src="stream.mjpg" width="{}" height="{}" />
+</body>
+</html>
+""".format(RESOLUTION_X, RESOLUTION_Y)
 
-        if video_file is not None:
-            cap = cv2.VideoCapture(video_file)
+
+class StreamingOutput(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
+        self.engine = None
+
+    def set_engine(self, engine):
+        self.engine = engine
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
+
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.headers.get('Authorization') is None:
+            self.do_AUTHHEAD()
+            self.wfile.write(b'no auth header received')
+        elif self.headers.get('Authorization') == BASIC_AUTH:
+            self.authorized_get()
         else:
-            cap = cv2.VideoCapture(cam_id)
-        cap.set(3, cam_width)
-        cap.set(4, cam_height)
-        success,frame = cap.read()
-        count = 0
-        while success:
-            #cv2.imwrite(r"C:\Users\lafacero\Desktop\opencv_test\images\frame%d.jpg" % count, image)     # save frame as JPEG file
-            success,frame = cap.read()
-            print ('Read frame_{}'.format(count), " ", success)
-            count += 1
+            self.do_AUTHHEAD()
+            self.wfile.write(b'not authenticated')
 
-            
-            frame = cv2.cvtColor(cv2_im,cv2.COLOR_BGR2RGB)
-            pil_image = Image.fromarray(frame)
-            pil_image.resize((641, 481), Image.NEAREST)
-            engine = PoseEngine('models/posenet_mobilenet_v1_075_481_641_quant_decoder_edgetpu.tflite')
-            poses, inference_time = engine.DetectPosesInImage(np.uint8(pil_image))
-            print('Inference time: %.fms' % inference_time)
+    def do_AUTHHEAD(self):
+        self.send_response(401)
+        self.send_header('WWW-Authenticate', 'Basic realm=\"picamera\"')
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
-            '''
-            for pose in poses:
-                if pose.score < 0.4: continue
-                print('\nPose Score: ', pose.score)
-                for label, keypoint in pose.keypoints.items():
-                    print(' %-20s x=%-4d y=%-4d score=%.1f' %
-                        (label, keypoint.yx[1], keypoint.yx[0], keypoint.score))
-            '''
-            encode_return_code, image_buffer = cv2.imencode('.jpg', frame) #TODO: draw lines on body parts!
-            io_buf = io.BytesIO(image_buffer)
-            yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + io_buf.read() + b'\r\n')
-            #cv2.imshow("video", frame)
-            #if cv2.waitKey(1) & 0xFF == ord('q'):
-            #    break
+    def append_objs_to_img(self, cv2_im, objs, labels):
+        height, width, channels = cv2_im.shape
+        for obj in objs:
+            x0, y0, x1, y1 = obj.bounding_box.flatten().tolist()
+            x0, y0, x1, y1 = int(x0*width), int(y0*height), int(x1*width), int(y1*height)
+            percent = int(100 * obj.score)
+            label = '%d%% %s' % (percent, labels[obj.label_id])
 
-@app.route('/pose_detection')
-def pose_detection():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(
-        pose_detect(),
-        mimetype='multipart/x-mixed-replace; boundary=frame'
-    )
+            cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
+            cv2_im = cv2.putText(cv2_im, label, (x0, y0+30),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
+        return cv2_im
+
+    def authorized_get(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                stream_video = io.BytesIO()
+                #_, width, height, channels = engine.get_input_tensor_shape()
+
+                while True:
+                    # getting image
+                    camera.capture(stream_video, 
+                                format='jpeg', 
+                                use_video_port=True)
+                    stream_video.truncate()
+                    stream_video.seek(0)
+                    
+
+                    # cv2 / PIL coding
+                    cv2_im = np.frombuffer(stream_video.getvalue(), dtype=np.uint8)
+                    cv2_im = cv2.imdecode(cv2_im, 1)
+                    pil_im = Image.fromarray(cv2_im)
+                    
+                    '''
+                    # object detection
+                    start_ms = time.time()
+                    objs = engine.detect_with_image(pil_im, threshold=args.threshold,
+                                    keep_aspect_ratio=True, relative_coord=True,
+                                    top_k=args.top_k)
+                    elapsed_ms = time.time() - start_ms
+
+                    cv2_im = self.append_objs_to_img(cv2_im, objs, labels)
+                    '''
+
+                    #pose detection
+                    pil_im.resize((641, 481), Image.NEAREST)
+                    engine = PoseEngine('/home/scripts/pose_detection/browser/models/posenet_mobilenet_v1_075_481_641_quant_decoder_edgetpu.tflite')
+                    poses, inference_time = engine.DetectPosesInImage(np.uint8(pil_im))
+
+                    #print points on shell
+                    for pose in poses:
+                        if pose.score < 0.4: continue
+                        print('\nPose Score: ', pose.score)
+                        for label, keypoint in pose.keypoints.items():
+                            print(' %-20s x=%-4d y=%-4d score=%.1f' %
+                                (label, keypoint.yx[1], keypoint.yx[0], keypoint.score))
+                    
+
+                    #Draw lines between points
+                    for pose in poses:
+                        if pose.score < 0.4:
+                            for part in pose.keypoints.keys():
+                                lineThickness=2
+                                cv2.line(cv2_im, (pose.keypoints['nose'].yx[1], pose.keypoints['nose'].yx[0]), (pose.keypoints['right wrist'].yx[1], pose.keypoints['right wrist'].yx[0]), [0, 255, 0], lineThickness)
+                                #TODO: must connect other parts
+
+                    r, buf = cv2.imencode(".jpg", cv2_im)
+                    #Show on browser
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-type','image/jpeg')
+                    self.send_header('Content-length',str(len(buf)))
+                    self.end_headers()
+                    self.wfile.write(bytearray(buf))
+                    self.wfile.write(b'\r\n')
+                    
+
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, threaded=True)
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+
+if __name__ == '__main__':
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', help='File path of Tflite model.', required=True)
+    parser.add_argument('--label', help='File path of label file.', required=True)
+    parser.add_argument('--top_k', type=int, default=3,
+                        help='number of classes with highest score to display')
+    parser.add_argument('--threshold', type=float, default=0.3,
+                        help='class score threshold')
+
+    args = parser.parse_args()
+    '''
+    res = '{}x{}'.format(RESOLUTION_X, RESOLUTION_Y)
+
+    '''
+    with open(args.label, 'r') as f:
+        pairs = (l.strip().split(maxsplit=1) for l in f.readlines())
+        labels = dict((int(k), v) for k, v in pairs)
+    '''
+    #engine = DetectionEngine(args.model)
+
+    with picamera.PiCamera(resolution=res, framerate=FRAMERATE, sensor_mode=2) as camera:
+        camera.hflip = HFLIP
+        camera.vflip = VFLIP
+        camera.rotation = ROTATION
+
+        try:
+            address = ('', 8080)
+            server = StreamingServer(address, StreamingHandler)
+            server.serve_forever()
+        except:
+            print("error on the server!")
